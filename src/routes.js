1c05851d5b9cdf02117eb70d25dbeff648324b86
Antonio Hamilton <antoniohamiltonfreitas@outlook.com>
2020-03-07 00:12:22 -0300

    Padronização do projeto

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+
+const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      <></>
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
