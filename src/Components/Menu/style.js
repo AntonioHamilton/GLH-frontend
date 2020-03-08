90ad507c14b34b01dfc17fb3c783bf74ba18699a
Lais Vitoria <laisvitoria0607@gmail.com>
2020-03-08 04:43:27 -0300

    aceitando modificações

--- src/Components/Menu/style.js
+++ src/Components/Menu/style.js
@@ -1,17 +1,11 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  position: sticky;
   background: #ffffff;
   height: 75px;
   justify-content: space-between;
   display: flex;
-<<<<<<< HEAD
   
-=======
-  padding: 0px 9vw;
-  top: 0;
->>>>>>> 1ece09296c228575f42bf100b8cd4db312a6ce7c
 `;
 
 export const DivText = styled.div`
@@ -19,17 +13,10 @@ export const DivText = styled.div`
   justify-content: flex-end;
   align-items: center;
   margin-right: 150px;
-  
 `;
 
-<<<<<<< HEAD
-export const Text = styled.a`  
-font-weight: bold;
-=======
 export const Text = styled.a`
-  text-decoration: none;
   font-weight: bold;
->>>>>>> 1ece09296c228575f42bf100b8cd4db312a6ce7c
   color: #000;
   background-color: #fff;
   height: 100%;
@@ -53,6 +40,7 @@ export const Divisor = styled.h2`
   text-align: center;
 `;
 export const Logo = styled.img`
+  widht: 100px;
   height: 75px;
   margin-left: 150px;
 `;
